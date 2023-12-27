0329e30171311c3114ba71e8efbacbd61adb0ff3
francisco <fjalil00@gmail.com>
2023-12-26 21:52:19 -0300

    se modifico seccion de imagenes

--- src/componentes/Itemcard/Itemcard.jsx
+++ src/componentes/Itemcard/Itemcard.jsx
@@ -4,13 +4,13 @@ const Itemcard = ({item}) => {
   
     return (
     <article  className="w-50">
-    <img src={item.img} alt={ImageBitmapRenderingContext.name} className="flex-auto"/>
+    <img src={item.img} alt={item.name} className="flex-auto"/>
     <h3 className="text-2x1 font-semibold">{item.name}</h3>
     <hr/>
     <p>{item.description}</p>
     <p className="text-xl font-mono">Precio: ${item.price}</p>
     <Boton>
-   <Link to={'/item/${item.id}'}> ver mas</Link>
+   <Link to={'/item/${}'}> ver mas</Link>
     </Boton>
 </article>
   )
